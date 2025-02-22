bf648a4c448d8aa67f4ef1c8b95911e6e2b1a12b
Suryansh160 <suryanshgupta160@gmail.com>
2025-02-22 20:55:21 +0530

    made a real form for costum inputs

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -2,20 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
+import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
+import './edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
-  const UserProductsScreen({super.key});
+  static const routeName = '/user-products';
 
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
 
     return Scaffold(
+      drawer: AppDrawer(),
       appBar: AppBar(
         title: const Text('Your products'),
         actions: <Widget>[
           IconButton(
-            onPressed: null,
+            onPressed: () =>
+                Navigator.of(context).pushNamed(EditProductScreen.routeName),
             icon: const Icon(Icons.add),
           ),
         ],
@@ -23,7 +28,13 @@ class UserProductsScreen extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(10),
         child: ListView.builder(
-          itemBuilder: (_),
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                  productsData.items[i].title, productsData.items[i].imageUrl),
+              Divider(),
+            ],
+          ),
           itemCount: productsData.items.length,
         ),
       ),
